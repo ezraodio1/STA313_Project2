70a2e41b84e73d2f2970474b0ecf7f123b1379de
@rawalmalika <malika.rawal@duke.edu>
2024-04-14 13:14:37 -0400

    adding UI components to app

--- NorthCarolinaApp/app.R
+++ NorthCarolinaApp/app.R
@@ -1,50 +1,47 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(leaflet)
 
-# Define UI for application that draws a histogram
+# Define UI for application
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("North Carolina Election Data"),
+    leafletOutput("map"),
+    
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+      sidebarPanel(
+        selectInput("county", "Choose a County to View:",
+                    choices = election_data$County,
+                    selected = election_data$County[1]),
+        
+        sliderInput("year",
+                    "Election Year:",
+                    min = 2000,
+                    max = 2024,
+                    value = 4)
+      ),
 
-        # Show a plot of the generated distribution
         mainPanel(
            plotOutput("distPlot")
         )
-    )
+)
 )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$map <- renderLeaflet({
+    leaflet() |>
+      addTiles() 
+  })
+  
+  observe({
+    county_picked <- election_data[election_data$County == input$county,]
+    lat <- county_picked$Latitude
+    long <- county_picked$Longitude
+    
+    leafletProxy("map") |>
+      setView(lng = long, lat = lat, zoom = 8)
+  })
 }
 
 # Run the application 
