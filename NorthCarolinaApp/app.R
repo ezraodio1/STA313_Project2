82aeba37487b35f4858b2b56fe4eceb4b022c182
ezraodio1 <ezra.odio@duke.edu>
2024-04-17 13:11:03 -0400

    Added an option to clear selections for county dropdown and made it so selected county is highlighted

--- NorthCarolinaApp/app.R
+++ NorthCarolinaApp/app.R
@@ -14,7 +14,7 @@ election_data_combined <- read_csv("data for app/election_data_wide_geo.csv")
 counties_geo <- st_read("data for app/nc_counties.geojson")
 
 counties_geo <- counties_geo |>
-mutate(County = str_to_upper(County))
+  mutate(County = str_to_upper(County))
 
 #Change datatype to numeric
 election_data_combined$Lat <- as.numeric(as.character(election_data_combined$Lat))
@@ -22,21 +22,22 @@ election_data_combined$Long <- as.numeric(as.character(election_data_combined$Lo
 
 # Define UI for application
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("North Carolina Election Data"),
-    leafletOutput("map"),
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("county", "Choose a County to View:",
-                    choices = election_data_combined$County,
-                    selected = election_data_combined$County[1]),
-        sliderInput("year",
-                   "Election Year:",
-                   min = 2000,
-                   max = 2024,
-                   value = 2020),
-    
+  
+  # Application title
+  titlePanel("North Carolina Election Data"),
+  leafletOutput("map"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("county", "Choose a County to View:",
+                  choices = c("None" = "", election_data_combined$County),
+                  selected = ""),
+      #EZRA: Got rid of slider - it seemed redundant
+      # sliderInput("year",
+      #             "Election Year:",
+      #             min = 2000,
+      #             max = 2024,
+      #             value = 2020),
+      
       selectInput("political", "Choose a Political Party: ",
                   choices = c("Rep" = "Rep", "Dem" = "Dem"),
                   selected = "Rep"),
@@ -44,13 +45,13 @@ ui <- fluidPage(
                   "Election Year:",
                   choices = c("2000", "2004", "2008", "2012", "2016", "2020"),
                   selected = "2020")
-      ),
-      
-
-        mainPanel(
-           plotOutput("distPlot") #Placeholder, not actually using a histogram
-        )
-)
+    ),
+    
+    
+    mainPanel(
+      plotOutput("distPlot") #Placeholder, not actually using a histogram
+    )
+  )
 )
 
 
@@ -79,36 +80,39 @@ server <- function(input, output, session) {
   })
   
   observe({
-    df <- picking_political()
-    
-    matched_index <- match(counties_geo$County, df$County)
-    matched_votes <- df$Votes[matched_index]
-    
-    palette <- colorQuantile(if (input$political == "Rep") "Reds" else "Blues", na.omit(matched_votes), n = 5)
-    
-    leafletProxy("map", data = counties_geo) |>
-      clearShapes() |>
-      addPolygons(
-        fillColor = ~palette(matched_votes),
-        fillOpacity = 0.8,
-        color = "#2B2B2B",
-        weight = 1,
-        popup = ~paste(County, "<br>", input$political, "Votes: ", matched_votes)
-      )
-  })
+      df <- picking_political()
+      
+      matched_index <- match(counties_geo$County, df$County)
+      matched_votes <- df$Votes[matched_index]
+      selected_county <- input$county
+      
+      palette <- colorQuantile(if (input$political == "Rep") "Reds" else "Blues", na.omit(matched_votes), n = 5)
+      
+      leafletProxy("map", data = counties_geo) |>
+        clearShapes() |>
+        addPolygons(
+          fillColor = ~palette(matched_votes),
+          fillOpacity = ~ifelse(County == selected_county, 1, 0.5),
+          color = ~ifelse(County == selected_county, "black", "gray"),
+          opacity = 1,
+          smoothFactor = 0,
+          weight = ~ifelse(County == selected_county, 3, 1),
+          popup = ~paste(County, "<br>", input$political, "Votes: ", matched_votes)
+        )
+    })
   
   #observe({
   #  selected_county <- counties_geo[counties_geo$County == input$county]
   #  county_centroid <- st_centroid(st_geometry(selected_county))
-    
-    # Pulling the long/lat
+  
+  # Pulling the long/lat
   #  lng <- st_coordinates(county_centroid)[1, 1]
   #  lat <- st_coordinates(county_centroid)[1, 2]
-    
-    # Use leafletProxy to update the existing leaflet map
+  
+  # Use leafletProxy to update the existing leaflet map
   #  leafletProxy("map", data = selected_county) %>%
   #   setView(lng = lng, lat = lat, zoom = 10)
-#})
+  #})
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
