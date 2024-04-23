4a645aef0eac83a6c829157d3fef8b7503b41676
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2024-04-23 10:23:11 -0400

    Move app to root of repo

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ counties_geo <- st_read("data for app/nc_counties.geojson")
 counties_geo <- counties_geo |>
   mutate(County = str_to_upper(County))
 
-election_data_combined 
+election_data_combined
 
 # Change datatype to numeric
 election_data_combined$Lat <- as.numeric(as.character(election_data_combined$Lat))
@@ -27,24 +27,24 @@ election_data_combined$Long <- as.numeric(as.character(election_data_combined$Lo
 # UI ---------------------------------------------------------------------------
 
 ui <- fluidPage(
-  
+
   # Application title
   titlePanel("North Carolina Election Data"),
   leafletOutput("map"),
   sidebarLayout(
     sidebarPanel(
       selectInput("county", "Choose a County to View:",
-                  choices = c("None" = "", election_data_combined$County),
-                  selected = ""),
+        choices = c("None" = "", election_data_combined$County),
+        selected = ""
+      ),
       selectInput("year",
-                  "Election Year:",
-                  choices = c("2000", "2004", "2008", "2012", "2016", "2020"),
-                  selected = "2020")
+        "Election Year:",
+        choices = c("2000", "2004", "2008", "2012", "2016", "2020"),
+        selected = "2020"
+      )
     ),
-    
-    
     mainPanel(
-      plotOutput("distPlot") #Placeholder, not actually using a histogram
+      plotOutput("distPlot") # Placeholder, not actually using a histogram
     )
   )
 )
@@ -52,60 +52,61 @@ ui <- fluidPage(
 # server -----------------------------------------------------------------------
 
 server <- function(input, output, session) {
- 
   combining_rep_dem <- reactive({
     election_data_combined |>
       filter(Year == as.numeric(input$year)) |>
       select(County, rep_per_capita, dem_per_capita, Votes_REP, Votes_DEM) |>
       mutate(politicalparty = (dem_per_capita - rep_per_capita) / (dem_per_capita + rep_per_capita))
   })
-    
+
   output$map <- renderLeaflet({
     leaflet(data = counties_geo) |>
       addTiles() |>
-      setView(lng = -79.0, lat =35.5, zoom = 7)
+      setView(lng = -79.0, lat = 35.5, zoom = 7)
   })
-  
+
   observe({
     df <- combining_rep_dem()
-    
+
     matched_index <- match(counties_geo$County, df$County)
     matched_political <- df$politicalparty[matched_index]
     selected_county <- input$county
     matched_votes_rep <- df$Votes_REP[matched_index]
     matched_votes_dem <- df$Votes_DEM[matched_index]
-    
+
     # Colorblind friendly colors: https://davidmathlogic.com/colorblind/#%23D81B60-%231E88E5-%23FFC107-%23004D40
     # Do we need to make this colorblind friendly? Hard to see the difference between red and blue if so
-    
+
     palette <- colorNumeric(c("red", "white", "blue"), domain = range(matched_political, na.rm = TRUE))
     leafletProxy("map", data = counties_geo) |>
       clearShapes() |>
       addPolygons(
-        fillColor = ~palette(matched_political),
-        fillOpacity = ~ifelse(County == selected_county, 1, 0.5),
-        color = ~ifelse(County == selected_county, "black", "gray"),
+        fillColor = ~ palette(matched_political),
+        fillOpacity = ~ ifelse(County == selected_county, 1, 0.5),
+        color = ~ ifelse(County == selected_county, "black", "gray"),
         opacity = 1,
         smoothFactor = 0,
-        weight = ~ifelse(County == selected_county, 3, 1),
-        popup = ~paste(County, "<br>", input$political,
-                       "Republican Votes: ", matched_votes_rep, "<br>",
-                       "Democrat Votes: ", matched_votes_dem, "<br>")
+        weight = ~ ifelse(County == selected_county, 3, 1),
+        popup = ~ paste(
+          County, "<br>", input$political,
+          "Republican Votes: ", matched_votes_rep, "<br>",
+          "Democrat Votes: ", matched_votes_dem, "<br>"
+        )
       )
   })
-  
+
   observe({
     df <- combining_rep_dem()
     matched_political <- df$politicalparty
-    
+
     palette <- colorNumeric(c("red", "white", "blue"), domain = range(matched_political, na.rm = TRUE))
     leafletProxy("map", data = counties_geo) |>
-      
-      addLegend("bottomright", pal = palette, values = ~matched_political,
-                title = "Republican to Democrat Scale Per Capita",
-                opacity = 0.7)  
+      addLegend("bottomright",
+        pal = palette, values = ~matched_political,
+        title = "Republican to Democrat Scale Per Capita",
+        opacity = 0.7
+      )
   })
-  
 }
 
 # run app ----------------------------------------------------------------------
